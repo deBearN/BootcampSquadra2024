ffdac1bcbda8a1f4a3ee198ef6e67bdd97475a1f
Francisco Queiroz <francisco.queiroz@squadra.com.br>
2024-12-06 07:58:55 -0300

    Configuração swagger

--- ApiCRUD/ApiCRUD/Program.cs
+++ ApiCRUD/ApiCRUD/Program.cs
@@ -1,5 +1,6 @@
 using ApiCRUD.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,50 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "Bootcamp .NET C#",
+            Version = "2024.2",
+            Description = "API CRUD"
+        });
+
+    string applicationBasePath = AppContext.BaseDirectory;
+    string applicationName = AppDomain.CurrentDomain.FriendlyName;
+    string xmlDocumentPath = Path.Combine(applicationBasePath, $"{applicationName}.xml");
+
+    if (File.Exists(xmlDocumentPath))
+    {
+        options.IncludeXmlComments(xmlDocumentPath);
+    }
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Insira o token JWT desta maneira: Bearer SEU_TOKEN",
+        Name = "Authorization",
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+});
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
